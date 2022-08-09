e4f16fc0bef48706336c08f2aa0918a7c81c9ffe
kajnorman <knn@eaaa.dk>
2022-08-09 08:50:20 +0200

    Create gpiotest.py

---
+++ gpiotest.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import time
+
+
+sys.path.append("/home/ubuntu/Robotics/QuadrupedRobot")
+sys.path.extend([os.path.join(root, name) for root, dirs, _ in os.walk("/home/ubuntu/Robotics/QuadrupedRobot") for name in dirs])
+
+import Mangdang.Adafruit_GPIO as GPIO
+import GPIO as GPIO
+
+gpio =  GPIO.get_platform_gpio()
+
+gpio.setup(19,0)  # 0 for out
+
+while True:
+   gpio.set_high(19)
+   time.sleep(1)
+   gpio.set_low(19)
+   time.sleep(1)
